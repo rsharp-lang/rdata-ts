0faa8a57b08eac7d4ead0f93fc4a93cea98841c5
xieguigang <xie.guigang@gcmodeller.org>
2022-02-04 17:19:46 +0800

    LengthRewriter

--- rdata/src/transforms.ts
+++ rdata/src/transforms.ts
@@ -1,36 +1,37 @@
-function LengthRewriter(length, options) {
-  if (!(this instanceof LengthRewriter)) {
-    return new LengthRewriter(length, options);
+class LengthRewriter extends Transform {
+
+  public written_count: number;
+  public length: number;
+
+  public constructor(length, options = { objectMode: false }) {
+    super(options);
+
+    options.objectMode = false;
+
+    this.written_count = 0;
+    this.length = length;
   }
 
-  if (!options) {
-    options = {};
-  }
-  options.objectMode = false;
-  this.written_count = 0;
-  this.length = length;
-  Transform.call(this, options);
+  _transform(buf, encoding, callback) {
+    let written_count = this.written_count;
+
+    if (written_count <= 4) {
+      let offset = 4 - written_count;
+      if (offset >= 0 && offset < buf.length) {
+        buf.writeInt32BE(this.length || 0, offset);
+      }
+      written_count += buf.length;
+    }
+
+    this.written_count = written_count;
+    this.push(buf);
+
+    callback();
+  };
 }
 
-inherits(LengthRewriter, Transform);
 
-LengthRewriter.prototype._transform = function _transform(buf, encoding, callback) {
 
-  let written_count = this.written_count;
-
-  if (written_count <= 4) {
-    let offset = 4 - written_count;
-    if (offset >= 0 && offset < buf.length) {
-      buf.writeInt32BE(this.length || 0, offset);
-    }
-    written_count += buf.length;
-  }
-
-  this.written_count = written_count;
-
-  this.push(buf);
-  callback();
-};
 
 function KeyExtractor(key, options) {
   if (!(this instanceof KeyExtractor)) {
