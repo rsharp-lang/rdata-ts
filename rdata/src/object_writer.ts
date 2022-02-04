bb3609d70035fc515f5e6bf68fdf010fa585e6c0
xieguigang <xie.guigang@gcmodeller.org>
2022-02-04 17:41:27 +0800

    object writer

--- rdata/src/object_writer.ts
+++ rdata/src/object_writer.ts
@@ -1,25 +1,27 @@
-function ObjectWriter(stream, options) {
-  options = options || {};
-  if (options.gzip) {
-    let gz = zlib.createGzip();
-    gz.pipe(stream);
-    this.stream = gz;
-  } else {
-    this.stream = stream;
-  }
+class ObjectWriter {
+
+    public constructor(public stream, options = { gzip: true }) {
+        if (options.gzip) {
+            let gz = zlib.createGzip();
+            gz.pipe(stream);
+            this.stream = gz;
+        } else {
+            this.stream = stream;
+        }
+    }
+
+    write(buffer) {
+        this.stream.write(buffer);
+    };
+
+    finish() {
+        let self = this;
+
+        return new Promise(function (resolve, reject) {
+            self.stream.on("close", resolve);
+            self.stream.on("finish", resolve);
+            self.stream.on("error", reject);
+            self.stream.end();
+        });
+    };
 }
-
-ObjectWriter.prototype.write = function (buffer) {
-  this.stream.write(buffer);
-};
-
-ObjectWriter.prototype.finish = function () {
-  let self = this;
-  return new Promise(function (resolve, reject) {
-    self.stream.on("close", resolve);
-    self.stream.on("finish", resolve);
-    self.stream.on("error", reject);
-    self.stream.end();
-  });
-};
-
--- rdata/src/package.ts
+++ rdata/src/package.ts
@@ -1,14 +1,14 @@
 namespace package {
 
-  export function generate_description(filedata, prefix) {
-    let title = filedata.title;
-    let version = filedata.version;
-    if (prefix) {
-      prefix = `${prefix}.`;
-    }
-    let now = new Date().toISOString().split('T')[0];
-    let date = now;
-    let description = `\
+    export function generate_description(filedata, prefix) {
+        let title = filedata.title;
+        let version = filedata.version;
+        if (prefix) {
+            prefix = `${prefix}.`;
+        }
+        let now = new Date().toISOString().split('T')[0];
+        let date = now;
+        let description = `\
 Package: ${prefix}${title}
 Version: ${version}
 Date: ${date}
@@ -17,20 +17,20 @@ Description: ${title}
 Title: ${title}
 LazyData: yes
 NeedsCompilation: yes`;
-    return description;
-  };
+        return description;
+    };
 
-  export function create_package(filedata, package_info) {
-    let data_filename = package_info.data_filename;
-    let description = package_info.description;
-    let package_prefix = package_info.prefix || '';
-    let gz = zlib.createGzip();
-    let archive = archiver('tar', { store: true });
-    archive.pipe(gz);
-    archive.append(fs.createReadStream(filedata.path), { name: `${filedata.title}/data/${data_filename}.rda` });
-    archive.append('', { name: `${filedata.title}/NAMESPACE` });
-    archive.append(generate_description(filedata, package_prefix), { name: `${filedata.title}/DESCRIPTION` });
-    archive.finalize();
-    return gz;
-  };
+    export function create_package(filedata, package_info) {
+        let data_filename = package_info.data_filename;
+        let description = package_info.description;
+        let package_prefix = package_info.prefix || '';
+        let gz = zlib.createGzip();
+        let archive = archiver('tar', { store: true });
+        archive.pipe(gz);
+        archive.append(fs.createReadStream(filedata.path), { name: `${filedata.title}/data/${data_filename}.rda` });
+        archive.append('', { name: `${filedata.title}/NAMESPACE` });
+        archive.append(generate_description(filedata, package_prefix), { name: `${filedata.title}/DESCRIPTION` });
+        archive.finalize();
+        return gz;
+    };
 }
